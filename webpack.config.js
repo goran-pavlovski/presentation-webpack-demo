484a8ee030c9ecac919a27bf8830a05022c182de
Goran <goran.pavlovski@it-labs.com>
2020-10-25 22:21:26 +0100

    5.2. Setting up webpack-plugin-serve as an alternative to WDS
    - install webpack-plugin-serve as dev dependency
    - configure webpack.config.js to use it

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
 const { mode } = require('webpack-nano/argv');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ["./src/index.js", "webpack-plugin-serve/client"],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   mode,
   watch: mode === 'development',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     index: 'index.html',
-    port: 9000
+    port: 9000,
+    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
-    })
-  ]
-}
+      template: './index.html',
+    }),
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: './dist',
+      liveReload: true,
+      waitForBuild: true,
+      open: true
+    }),
+  ],
+};
